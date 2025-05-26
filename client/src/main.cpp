6f7216d9e6f309ffd5d9d59588a7d42f62354c02
Santiago Freire <santiago.freire@opendeusto.es>
2025-05-26 20:18:50 +0200

    comunicación bidireccional y mejora implementación del handler del servidor

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -23,7 +23,7 @@ int main() {
     std::cerr << "¡Error conectando al servidor!" << std::endl;
     return 1;
   }
-  int result = socket.send_message("1\\jose\\yoquese");
+  int result = socket.send_message("1\\2\\yoquese");
   if (result <= 0) {
     std::cerr << "Error al enviar el mensaje: " << WSAGetLastError()
               << std::endl;
@@ -31,6 +31,7 @@ int main() {
     std::cout << "Mensaje enviado correctamente" << std::endl;
   }
 
+  auto ignore = socket.receive_message();
   socket.close();
 
   return 0;
